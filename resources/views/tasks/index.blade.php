6880b49fc17b6a6f10a8775591a2c4d67f307b69
Reiikuzoo <ryfaryt02@gmail.com>
2025-04-21 21:15:31 +0700

    ubah logo

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do List</title>
-    <link rel="icon" type="image/png" href="{{ asset('image/logo todolist.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('image/logotdl.png') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
